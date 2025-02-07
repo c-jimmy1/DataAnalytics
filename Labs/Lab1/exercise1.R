38b7eb84042c50124fc62b334989850747ada3e3
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2025-02-07 10:33:48 -0500

    init commit lab 2

--- Labs/Lab1/exercise1.R
+++ Labs/Lab1/exercise1.R
@@ -1,34 +1,34 @@
+# Reading Data
 EPI_data <- read.csv("epi2024results06022024.csv")
 
 attach(EPI_data)
 
 EPI.new
 
+#Cleaning Data
 NAs <- is.na(EPI.new)
 
 EPI.new.noNAs <- EPI.new[!NAs]
 
+# Exercise 1
 summary(EPI.new)
-
 fivenum(EPI.new,na.rm=TRUE)
-
 stem(EPI.new)
-
 hist(EPI.new)
 
-#hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
+hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
 
-#lines(density(EPI.new,na.rm=TRUE,bw=1.))
+lines(density(EPI.new,na.rm=TRUE,bw=1.))
 
-#rug(EPI.new)
+rug(EPI.new)
 
-#boxplot(EPI.new, APO.new)
+boxplot(EPI.new, APO.new)
 
-#hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
+hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
 
-#lines (density(EPI.new,na.rm=TRUE,bw=1.))
+lines (density(EPI.new,na.rm=TRUE,bw=1.))
 
-#rug(EPI.new)
+rug(EPI.new)
 
 hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
 
@@ -42,3 +42,33 @@ lines(x,q)
 lines(x,.4*q) 
 q<-dnorm(x,mean=65, sd=5,log=FALSE) 
 lines(x,.12*q) 
+
+# Exercise 2
+
+plot(ecdf(EPI.new), do.points=FALSE, verticals=TRUE) 
+qqnorm(EPI.new); qqline(EPI.new) 
+qqplot(rnorm(250), EPI.new, xlab = "Q-Q plot for norm dsn") 
+qqline(EPI.new)
+
+qqplot(rt(250, df = 5), EPI.new, xlab = "Q-Q plot for t dsn") 
+qqline(EPI.new)
+
+
+plot(ecdf(APO.new), do.points=FALSE, verticals=TRUE) 
+qqnorm(APO.new); qqline(APO.new) 
+qqplot(rnorm(250), APO.new, xlab = "Q-Q plot for norm dsn") 
+qqline(APO.new)
+
+qqplot(rt(250, df = 5), APO.new, xlab = "Q-Q plot for t dsn") 
+qqline(APO.new)
+
+
+plot(ecdf(WRS.new), do.points=FALSE, verticals=TRUE) 
+qqnorm(WRS.new); qqline(WRS.new) 
+qqplot(rnorm(250), WRS.new, xlab = "Q-Q plot for norm dsn") 
+qqline(WRS.new)
+
+qqplot(rt(250, df = 5), WRS.new, xlab = "Q-Q plot for t dsn") 
+qqline(WRS.new)
+
+
